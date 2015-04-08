da4f622c6e0e287d6a80d8678768ed5f97831e70
morpheus <e.stephant@iia-laval.fr>
2015-04-08 13:56:12 +0200

    Création branche perso morph

---
+++ app/cache/dev/twig/16/ed/be64883c8f66115bf559f96e5fc5dfb945fc6f0af91eac6b8e1ec0995c5f.php
@@ -0,0 +1,39 @@
+<?php
+
+/* MegaCastingBundle:Default:index.html.twig */
+class __TwigTemplate_16edbe64883c8f66115bf559f96e5fc5dfb945fc6f0af91eac6b8e1ec0995c5f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "MegaCastingBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
