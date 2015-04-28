3558fb111149e52bf4ba0c49ff9983a21eb99169
jeoff09 <jeoffrey.godart@hotmail.fr>
2015-04-28 22:07:42 +0200

    Mise en place Controller

--- src/MegaCastingBundle/Controller/offreMegaCastingcontroller.php
+++ src/MegaCastingBundle/Controller/offreMegaCastingcontroller.php
@@ -1,13 +1,16 @@
 <?php
 
+
+
 namespace MegaCastingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class offreController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('MegaCastingBundle:Default:index.html.twig', array('name' => $name));
     }
 }
+
